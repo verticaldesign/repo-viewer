e280d9adeb1c78d3849cb634eb44babb474e4fb4
erictuftee <erictuftee@gmail.com>
2018-09-21 13:29:54 -0500

    feat: print out list of repos

--- src/js/populateRepoList.js
+++ src/js/populateRepoList.js
@@ -1,3 +1,16 @@
-export function populateRepoList(list) {
-  console.table(list);
+export function populateRepoList(repoList) {
+  //console.table(repoList);
+  let simplifiedList = [...repoList].map(repo => {
+    return repo.name;
+  });
+  let list = document.createElement("ul");
+
+  simplifiedList.map(row => {
+    let newRow = document.createElement("li");
+    newRow.innerHTML = `${row}`;
+    list.appendChild(newRow);
+  });
+  console.log(list);
+  return list;
+  console.table(simplifiedList);
 }
--- src/js/repoContainer.js
+++ src/js/repoContainer.js
@@ -1,13 +1,14 @@
 import dataFetch from "./dataFetcher.js";
 import { populateRepoList } from "./populateRepoList.js";
 
-const dataURL = "https://api.github.com/users/verticaldesign/repos";
-
-let repoList = [];
-dataFetch(dataURL).then(repoList => {
-    populateRepoList(repoList);
-});
-
 export default () => {
-  console.log("test5");
+  let containerDiv = document.createElement("div");
+  containerDiv.innerHTML = "Text";
+
+  const dataURL = "https://api.github.com/users/verticaldesign/repos";
+  dataFetch(dataURL).then(repoList => {
+    containerDiv.appendChild(populateRepoList(repoList));
+    document.querySelector("body").appendChild(containerDiv);
+  });
+  //console.log("build");
 };
